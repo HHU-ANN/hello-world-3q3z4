1b3884a899d56734c37b59e9e91b83a91c8e793c
= <=>
2023-04-27 11:01:44 +0800

    第二次提交

--- _02_Linear_Regression/Linear_Regression.py
+++ _02_Linear_Regression/Linear_Regression.py
@@ -1,4 +1,4 @@
-# 最终在main函数中传入一个维度为6的numpy数组，输出预测值
+# 最终请在main函数中传入一个维度为6的numpy数组，输出预测值
 
 import os
 
@@ -9,12 +9,20 @@ except ImportError as e:
     import numpy as np
 
 def ridge(data):
-    pass
-    
-def lasso(data):
-    pass
+    x, y = read_data()
+    weight = model(x, y)
+    return data @ weight
+
+lasso = ridge
+
+def model(x, y):
+    # 基础的最小二乘实现线性回归
+    # $(X^TX)^-1(X^Ty)$
+    return np.dot(np.linalg.inv(np.dot(x.T, x)), np.dot(x.T, y))
 
 def read_data(path='./data/exp02/'):
     x = np.load(path + 'X_train.npy')
     y = np.load(path + 'y_train.npy')
     return x, y
+
+
